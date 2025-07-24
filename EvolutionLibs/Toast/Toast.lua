1770cdb102a58b81669e5a1796890a1f5652ee1a
NotFound <trongreygz@gmail.com>
2025-07-24 00:08:36 -0700

    Update Toast.lua

--- EvolutionLibs/Toast/Toast.lua
+++ EvolutionLibs/Toast/Toast.lua
@@ -1,14 +1,14 @@
 -- Toast/Toast.lua - Sistema de notificaciones tipo toast
 
 local BASE_URL = "https://raw.githubusercontent.com/Ades12121212121/EvolutionLibs/main/EvolutionLibs/"
-local Utils, Designs
+_G.EvoLibsCache = _G.EvoLibsCache or {}
 local function getUtils()
-    if not Utils then Utils = loadstring(game:HttpGet(BASE_URL .. "Utils/Utils.lua"))() end
-    return Utils
+    if not _G.EvoLibsCache.Utils then _G.EvoLibsCache.Utils = loadstring(game:HttpGet(BASE_URL .. "Utils/Utils.lua"))() end
+    return _G.EvoLibsCache.Utils
 end
 local function getDesigns()
-    if not Designs then Designs = loadstring(game:HttpGet(BASE_URL .. "Designs/Designs.lua"))() end
-    return Designs
+    if not _G.EvoLibsCache.Designs then _G.EvoLibsCache.Designs = loadstring(game:HttpGet(BASE_URL .. "Designs/Designs.lua"))() end
+    return _G.EvoLibsCache.Designs
 end
 
 -- Configuración del sistema de toast
