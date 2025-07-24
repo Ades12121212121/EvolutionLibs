6c529edba7f45356dfa242a89a9c65dfe2a4b8a1
NotFound <trongreygz@gmail.com>
2025-07-24 12:45:26 -0700

    Update Window.lua

--- EvolutionLibs/Window/Window.lua
+++ EvolutionLibs/Window/Window.lua
@@ -233,7 +233,7 @@ function Window.new(config)
 	end)
 
 	-- Sidebar
-	self.Sidebar = getSidebar().new(self.Main, {
+	self.Sidebar = Sidebar.new(self.Main, {
 		Theme = self.Theme,
 		Title = self.Config.Title or "Menu"
 	})
